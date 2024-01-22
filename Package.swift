31275fc65d7326764e73678a2465bd7d60dcb44b
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-01-22 15:55:10 -0800

    Added SwiftyJSON

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
             name: "StitchSchemaKit",
             targets: ["Schemas"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", exact: "5.0.1")
+    ],
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages this package depends on.

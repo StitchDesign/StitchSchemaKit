3828fcf3364b353b007f9a5d8e5fcf3578a83848
Nick Arner <nicholasarner@gmail.com>
2024-01-25 12:24:29 -0800

    Revert "Merge pull request #6 from vpl-codesign/nick-migration-3"
    This reverts commit 1fd8656d548cb820d0b4b11cbacd9937c62ccfd6, reversing
    changes made to d6becd0c4a4e97322f1bdb03d4c59ece4875e6a3.

--- Sources/StitchSchemaKit/SchemaVersions.swift
+++ Sources/StitchSchemaKit/SchemaVersions.swift
@@ -10,68 +10,19 @@ import SwiftUI
 
 // MARK: - UPDATE VERSION NUMBERS HERE
 public typealias CurrentStitchDocument = StitchDocument_V1
-public typealias CurrentDelayStyle = DelayStyle_V1
-public typealias CurrentDateAndTimeFormat_V1 = DateAndTimeFormat_V1
 public typealias CurrentNodeEntity = NodeEntity_V1
 public typealias CurrentPatchNodeEntity = PatchNodeEntity_V1
 public typealias CurrentLayerNodeEntity = LayerNodeEntity_V1
 public typealias CurrentNodePortInputEntity = NodePortInputEntity_V1
 public typealias CurrentSplitterNodeEntity = SplitterNodeEntity_V1
-public typealias CurrentNodeIOCoordinate = NodeIOCoordinate_V1
-public typealias CurrentGroupNodeIDCoordinate = GroupNodeID_V1
-public typealias CurrentSidebarLayerType = SidebarLayerType_V1
-public typealias CurrentNodeKind = NodeKind_V1
-public typealias CurrentAnchoring = Anchoring_V1
-public typealias CurrentCameraDirection = CameraDirection_V1
-public typealias CurrentLayerSize = LayerSize_V1
-public typealias CurrentLayerStroke = LayerStroke_V1
-public typealias CurrentLayerTextAlignment = LayerTextAlignment_V1
-public typealias CurrentLayerTextVerticalAlignment = LayerTextVerticalAligment_V1
-public typealias CurrentMediaKey = MediaKey_V1
-public typealias CurrentNetworkRequestType = NetworkRequestType_V1
-public typealias CurrentPlane = Plane_V1
-public typealias CurrentPoint3D = Point3D_V1
-public typealias CurrentPoint4D = Point4D_V1
-public typealias CurrentPortValueComparable = PortValueComparable_V1
-public typealias CurrentVisualMediaFitStyle = VisualMediaFitStyle_V1
-public typealias CurrentScrollDecelerationRate = ScrollDecelerationRate_V1
-public typealias CurrentScrollJumpStyle = ScrollJumpStyle_V1
-public typealias CurrentScrollMode = ScrollMode_V1
-public typealias CurrentLightType = LightType_V1
-public typealias CurrentTextTransform = TextTransform_V1
-
 // MARK: - end
 
-public typealias Anchoring = CurrentAnchoring.Anchoring
-public typealias CameraDirection = CurrentCameraDirection.CameraDirection
-public typealias DelayStyle = CurrentDelayStyle.DelayStyle
-public typealias DateAndTimeFormat = CurrentDateAndTimeFormat_V1.DateAndTimeFormat
-public typealias GroupNodeId = CurrentGroupNodeIDCoordinate.GroupNodeId
-public typealias LayerNodeEntity = CurrentLayerNodeEntity.LayerNodeEntity
-public typealias LayerSize = CurrentLayerSize.LayerSize
-public typealias LayerStroke = CurrentLayerStroke.LayerStroke
-public typealias LayerTextAlignment = CurrentLayerTextAlignment.LayerTextAlignment
-public typealias LayerTextVerticalAlignment = CurrentLayerTextVerticalAlignment.LayerTextVerticalAligment
-public typealias LightType = CurrentLightType.LightType
-public typealias MediaKey = CurrentMediaKey.MediaKey
-public typealias NetworkRequestType = CurrentNetworkRequestType.NetworkRequestType
-public typealias NodeEntity = CurrentNodeEntity.NodeEntity
-public typealias NodeKind = CurrentNodeKind.NodeKind
-public typealias NodeIOCoordinate = CurrentNodeIOCoordinate.NodeIOCoordinate
-public typealias NodePortInputEntity = CurrentNodePortInputEntity.NodePortInputEntity
-public typealias PatchNodeEntity = CurrentPatchNodeEntity.PatchNodeEntity
-public typealias Plane = CurrentPlane.Plane
-public typealias Point3D = CurrentPoint3D.Point3D
-public typealias Point4D = CurrentPoint4D.Point4D
-public typealias PortValueComparable = CurrentPortValueComparable.PortValueComparable
-public typealias ScrollDecelerationRate = CurrentScrollDecelerationRate.ScrollDecelerationRate
-public typealias ScrollJumpStyle = CurrentScrollJumpStyle.ScrollJumpStyle
-public typealias ScrollMode = CurrentScrollMode.ScrollMode
-public typealias SidebarLayerType = CurrentSidebarLayerType.SidebarLayerType
-public typealias SplitterNodeEntity = CurrentSplitterNodeEntity.SplitterNodeEntity
 public typealias StitchDocument = CurrentStitchDocument.StitchDocument
-public typealias TextTransform = CurrentTextTransform.TextTransform
-public typealias VisualMediaFitStyle = CurrentVisualMediaFitStyle.VisualMediaFitStyle
+public typealias NodeEntity = CurrentNodeEntity.NodeEntity
+public typealias PatchNodeEntity = CurrentPatchNodeEntity.PatchNodeEntity
+public typealias LayerNodeEntity = CurrentLayerNodeEntity.LayerNodeEntity
+public typealias NodePortInputEntity = CurrentNodePortInputEntity.NodePortInputEntity
+public typealias SplitterNodeEntity = CurrentSplitterNodeEntity.SplitterNodeEntity
 
 public enum StitchSchemaVersion: Int, VersionType {
     case _V1 = 1

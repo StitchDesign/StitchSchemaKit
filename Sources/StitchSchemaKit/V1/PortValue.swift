bc7d9e439a6056f9bcf4903cd1fa54a7bf008fb1
Nick Arner <nicholasarner@gmail.com>
2024-01-26 10:13:38 -0800

    JSONShapeKeys

--- Sources/StitchSchemaKit/V1/PortValue.swift
+++ Sources/StitchSchemaKit/V1/PortValue.swift
@@ -389,17 +389,3 @@ extension ShapeCommand: Codable {
         }
     }
 }
-
-public struct JSONShapeKeys {
-    public static let PATH = "path"
-    public static let TYPE = "type"
-
-    public static let POINT = "point"
-
-    public static let CLOSE_PATH = "closePath"
-    public static let MOVE_TO = "moveTo"
-    public static let LINE_TO = "lineTo"
-    public static let CURVE_TO = "curveTo"
-
-    public static let CURVE_FROM = "curveFrom"
-}

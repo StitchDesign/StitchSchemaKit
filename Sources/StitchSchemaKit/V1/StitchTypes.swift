bea6d3f3b62a30c223515ecdbc06df1a5a9ec11a
Nick Arner <nicholasarner@gmail.com>
2024-01-27 09:38:14 -0800

    CurrentCommentBoxData

--- Sources/StitchSchemaKit/V1/StitchTypes.swift
+++ Sources/StitchSchemaKit/V1/StitchTypes.swift
@@ -54,53 +54,6 @@ public enum UserVisibleType: String, CaseIterable, Codable {
 }
 
 
-public struct CommentBoxData: Codable, Equatable, Hashable {
-    public var id: CommentBoxId
-    
-    /*
-     "Which traversal level (group node) does this comment box belong to?"
-     
-     nil = top level
-     
-     TODO: implement this properly, but wait until after SwiftData since e.g. VisibleNodesViewModel etc. may be changing.
-     
-     For now we just make all comment boxes top-level.
-     */
-    public var groupId: GroupNodeId?
-    
-    public var title: String = "Comment"
-    public var color: Color
-    
-    public var nodes: NodeIdSet
-    
-    // TODO: potentially can consolidate CommentBoxData.position and CommentExpansionBox.startingPoint, CommentExpansionBox.anchorPoint, etc.;
-    // a little hard to tell, since the trig + gesture logic is complicated;
-    // low-priority refactor.
-    public var position: CGSize
-    public var previousPosition: CGSize
-    public var expansionBox: CommentExpansionBox
-    
-    public var zIndex: Double // = .zero
-    
-    public init(id: CommentBoxId = .init(),
-                groupId: GroupNodeId? = nil,
-                title: String = "Comment",
-                color: Color,
-                nodes: NodeIdSet = .init(),
-                position: CGSize = .zero,
-                previousPosition: CGSize = .zero,
-                expansionBox: CommentExpansionBox,
-                zIndex: Double) {
-        self.id = id
-        self.groupId = groupId
-        self.title = title
-        self.color = color
-        self.nodes = nodes
-        self.position = position
-        self.previousPosition = previousPosition
-        self.expansionBox = expansionBox
-        self.zIndex = zIndex
-    }
-}
+
 
 public typealias CommentBoxesDict = [CommentBoxId: CommentBoxData]

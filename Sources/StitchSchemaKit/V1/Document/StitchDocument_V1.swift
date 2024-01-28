1586958c2ea8d64652ef530ebe811fd5af1c77c0
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-01-28 15:51:24 -0800

    Renamed folders

---
+++ Sources/StitchSchemaKit/V1/Document/StitchDocument_V1.swift
@@ -0,0 +1,82 @@
+//
+//  StitchDocument.swift
+//  Stitch
+//
+//  Created by Elliot Boschwitz on 10/19/23.
+//
+
+import Foundation
+import SwiftUI
+
+public enum StitchDocument_V1: StitchSchemaVersionable {
+
+    // MARK: - ensure versions are correct
+    static let version = StitchSchemaVersion._V1
+    public typealias PreviousInstance = Self.StitchDocument
+    public typealias NodeEntitySchema = NodeEntity_V1
+    // MARK: - end
+
+    // TODO: transferable
+    public struct StitchDocument: StitchVersionedCodable {
+        public var projectId: ProjectId
+        public var name: String
+
+        // Preview window
+        public let previewWindowSize: CGSize
+        public let previewSizeDevice: PreviewSize
+        public let previewWindowBackgroundColor: Color
+
+        // Graph positioning data
+        public let localPosition: CGPoint
+        public let zoomData: CGFloat
+
+        // Node data
+        public let nodes: [NodeEntitySchema.NodeEntity]
+        public let orderedSidebarLayers: [SidebarLayerType]
+        public let commentBoxesDict: CommentBoxesDict
+
+        public let cameraSettings: CameraSettings
+
+        public init(projectId: ProjectId,
+             name: String,
+             previewWindowSize: CGSize,
+             previewSizeDevice: PreviewSize,
+             previewWindowBackgroundColor: Color,
+             localPosition: CGPoint,
+             zoomData: CGFloat,
+             nodes: [NodeEntitySchema.NodeEntity],
+             orderedSidebarLayers: [SidebarLayerType],
+             commentBoxesDict: CommentBoxesDict,
+             cameraSettings: CameraSettings) {
+            self.projectId = projectId
+            self.name = name
+            self.previewWindowSize = previewWindowSize
+            self.previewSizeDevice = previewSizeDevice
+            self.previewWindowBackgroundColor = previewWindowBackgroundColor
+            self.localPosition = localPosition
+            self.zoomData = zoomData
+            self.nodes = nodes
+            self.orderedSidebarLayers = orderedSidebarLayers
+            self.commentBoxesDict = commentBoxesDict
+            self.cameraSettings = cameraSettings
+        }
+        
+        // MARK: remove `transferRepresentation` from older `StitchDocument` versions
+//        static var transferRepresentation: some TransferRepresentation {
+//            FileRepresentation(contentType: .stitchDocument,
+//                               exporting: Self.exportDocument,
+//                               importing: Self.importDocument)
+//        }
+    }
+}
+
+extension StitchDocument_V1.StitchDocument {
+    public init(previousInstance: StitchDocument_V1.PreviousInstance) {
+        fatalError()
+        //            self.projectId = previousInstance.projectId
+        //            self.name = previousInstance.name
+        //            self.previewWindowSizeWidth = previousInstance.previewWindowSizeWidth
+        //            self.previewWindowSizeHeight = previousInstance.previewWindowSizeHeight
+        //            self.nodes = NodeEntitySchema.NodeEntity_Codable.upgradeEntities(previousInstance.nodes)
+    }
+}
---
+++ Sources/StitchSchemaKit/V1/Node/Group_V1/SplitterNodeEntity_V1.swift
@@ -0,0 +1,40 @@
+//
+//  SplitterNodeEntity_V1.swift
+//  Stitch
+//
+//  Created by Elliot Boschwitz on 12/30/23.
+//
+
+import Foundation
+
+public enum SplitterNodeEntity_V1: StitchSchemaVersionable {
+
+    // MARK: - ensure versions are correct
+    static var version = StitchSchemaVersion._V1
+    public typealias PreviousInstance = Self.SplitterNodeEntity
+    // MARK: - end
+
+    public struct SplitterNodeEntity {
+        public let id: NodeId
+        // Dates used to sort ports in group node view
+        public let lastModifiedDate: Date
+        public let type: SplitterType
+        
+        public init(id: NodeId, lastModifiedDate: Date, type: SplitterType) {
+            self.id = id
+            self.lastModifiedDate = lastModifiedDate
+            self.type = type
+        }
+    }
+}
+
+extension SplitterNodeEntity_V1.SplitterNodeEntity: StitchVersionedCodable {
+    public init(previousInstance: PatchNodeEntity_V1.PreviousInstance) {
+        fatalError()
+    }
+
+//    init(from viewModel: SplitterNodeEntity_V1.SplitterNodeEntity) {
+//        // Not actually used
+//        fatalError()
+//    }
+}

683401cd92f752b9b53a43b9e2aa6acce8beee47
Nick Arner <nicholasarner@gmail.com>
2024-01-29 11:29:30 -0800

    DateAndTimeFormat

--- Sources/StitchSchemaKit/V2/PortValueTypes/DateAndTimeFormat_V2.swift
+++ Sources/StitchSchemaKit/V2/PortValueTypes/DateAndTimeFormat_V2.swift
@@ -10,7 +10,7 @@ import Foundation
 public enum DateAndTimeFormat_V2: StitchSchemaVersionable {
     // MARK: - ensure versions are correct
     static var version: StitchSchemaVersion = StitchSchemaVersion._V2
-    public typealias PreviousInstance = Self.DateAndTimeFormat
+    public typealias PreviousInstance = DateAndTimeFormat_V1.DateAndTimeFormat
     // MARK: - endif
     
     public enum DateAndTimeFormat: String, CaseIterable {
@@ -20,7 +20,19 @@ public enum DateAndTimeFormat_V2: StitchSchemaVersionable {
 }
 
 extension DateAndTimeFormat_V2.DateAndTimeFormat: StitchVersionedCodable {
-    public init(previousInstance: DelayStyle_V2.PreviousInstance) {
-        fatalError()
+    public init(previousInstance: DateAndTimeFormat_V2.PreviousInstance) {
+        switch previousInstance {
+            
+        case .none:
+            self = .none
+        case .short:
+            self = .short
+        case .medium:
+            self = .medium
+        case .long:
+            self = .long
+        case .full:
+            self = .full
+        }
     }
 }

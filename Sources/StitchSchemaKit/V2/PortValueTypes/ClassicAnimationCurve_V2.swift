55716b04b212f97182ad18a9ef1375a9f4c258a0
Nick Arner <nicholasarner@gmail.com>
2024-01-29 10:46:03 -0800

    ClassicAnimationCurve

--- Sources/StitchSchemaKit/V2/PortValueTypes/ClassicAnimationCurve_V2.swift
+++ Sources/StitchSchemaKit/V2/PortValueTypes/ClassicAnimationCurve_V2.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  ClassicAnimationCurve.swift
+//
 //
 //  Created by Nicholas Arner on 1/26/24.
 //
@@ -10,7 +10,7 @@ import Foundation
 public enum ClassicAnimationCurve_V2: StitchSchemaVersionable {
     // MARK: - ensure versions are correct
     static var version: StitchSchemaVersion = StitchSchemaVersion._V2
-    public typealias PreviousInstance = Self.ClassicAnimationCurve
+    public typealias PreviousInstance = ClassicAnimationCurve_V1.ClassicAnimationCurve
     // MARK: - endif
  
 
@@ -37,6 +37,28 @@ public enum ClassicAnimationCurve_V2: StitchSchemaVersionable {
 
 extension ClassicAnimationCurve_V2.ClassicAnimationCurve: StitchVersionedCodable {
     public init(previousInstance: ClassicAnimationCurve_V2.PreviousInstance) {
-        fatalError()
+        switch previousInstance {
+            
+        case .linear:
+            self = .linear
+        case .quadraticIn:
+            self = .quadraticIn
+        case .quadraticOut:
+            self = .quadraticOut
+        case .quadraticInOut:
+            self = .quadraticInOut
+        case .sinusoidalIn:
+            self = .sinusoidalIn
+        case .sinusoidalOut:
+            self = .sinusoidalOut
+        case .sinusoidalInOut:
+            self = .sinusoidalInOut
+        case .exponentialIn:
+            self = .exponentialIn
+        case .exponentialOut:
+            self = .exponentialOut
+        case .exponentialInOut:
+            self = .exponentialInOut
+        }
     }
 }

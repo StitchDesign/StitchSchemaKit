85cf08884d905f7d0193e82052cf28b83db7a2de
Nick Arner <nicholasarner@gmail.com>
2024-01-29 15:16:55 -0800

    NodePortInputEntity_V1

--- Sources/StitchSchemaKit/V2/NodePort/PortValue_V2.swift
+++ Sources/StitchSchemaKit/V2/NodePort/PortValue_V2.swift
@@ -14,7 +14,7 @@ import SwiftyJSON
 public enum PortValue_V2: StitchSchemaVersionable {
     // MARK: - ensure versions are correct
     static let version = StitchSchemaVersion._V2
-    public typealias PreviousInstance = Self.PortValue
+    public typealias PreviousInstance = PortValue_V1.PortValue
     // MARK: - end
     
     public enum PortValue: Codable, Equatable {
@@ -65,6 +65,90 @@ public enum PortValue_V2: StitchSchemaVersionable {
 
 extension PortValue_V2.PortValue: StitchVersionedCodable {
     public init(previousInstance: PortValue_V2.PreviousInstance) {
-        fatalError()
+        switch previousInstance {
+            
+        case .string(let value):
+            self = .string(value)
+        case .bool(let value):
+            self = .bool(value)
+        case .int(let value):
+            self = .int(value)
+        case .number(let value):
+            self = .number(value)
+        case .layerDimension(let value):
+            self = .layerDimension(value)
+        case .matrixTransform(let value):
+            self = .matrixTransform(value)
+        case .plane(let value):
+            self = .plane(value)
+        case .networkRequestType(let value):
+            self = .networkRequestType(value)
+        case .color(let value):
+            self = .color(value)
+        case .size(let value):
+            self = .size(value)
+        case .position(let value):
+            self = .position(value)
+        case .point3D(let value):
+            self = .point3D(value)
+        case .point4D(let value):
+            self = .point4D(value)
+        case .pulse(let value):
+            self = .pulse(value)
+        case .asyncMedia(let value):
+            self = .asyncMedia(value)
+        case .json(let value):
+            self = .json(value)
+        case .anchoring(let value):
+            self = .anchoring(value)
+        case .cameraDirection(let value):
+            self = .cameraDirection(value)
+        case .assignedLayer(let value):
+            self = .assignedLayer(value)
+        case .scrollMode(let value):
+            self = .scrollMode(value)
+        case .textAlignment(let value):
+            self = .textAlignment(value)
+        case .textVerticalAlignment(let value):
+            self = .textVerticalAlignment(value)
+        case .fitStyle(let value):
+            self = .fitStyle(value)
+        case .animationCurve(let value):
+            self = .animationCurve(value)
+        case .lightType(let value):
+            self = .lightType(value)
+        case .layerStroke(let value):
+            self = .layerStroke(value)
+        case .textTransform(let value):
+            self = .textTransform(value)
+        case .dateAndTimeFormat(let value):
+            self = .dateAndTimeFormat(value)
+        case .shape(let value):
+            self = .shape(value)
+        case .scrollJumpStyle(let value):
+            self = .scrollJumpStyle(value)
+        case .scrollDecelerationRate(let value):
+            self = .scrollDecelerationRate(value)
+        case .comparable(let value):
+            self = .comparable(value)
+        case .delayStyle(let value):
+            self = .delayStyle(value)
+        case .shapeCoordinates(let value):
+            self = .shapeCoordinates(value)
+        case .shapeCommandType(let value):
+            self = .shapeCommandType(value)
+        case .shapeCommand(let value):
+            self = .shapeCommand(value)
+        case .orientation(let value):
+            self = .orientation(value)
+        case .cameraOrientation(let value):
+            self = .cameraOrientation(value)
+        case .deviceOrientation(let value):
+            self = .deviceOrientation(value)
+        case .vnImageCropOption(let value):
+            self = .vnImageCropOption(value)
+        case .none:
+            self = .none
+        }
     }
 }

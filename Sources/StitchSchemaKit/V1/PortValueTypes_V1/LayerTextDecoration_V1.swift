ea8d524f9406ec74598bd0824f141e4641240ecc
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-02-04 17:20:47 -0800

    Merged changes with main

---
+++ Sources/StitchSchemaKit/V1/PortValueTypes_V1/LayerTextDecoration_V1.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Elliot Boschwitz on 2/3/24.
+//
+
+import Foundation
---
+++ Sources/StitchSchemaKit/V1/PortValueTypes_V1/StitchFontChoice_V1.swift
@@ -0,0 +1,8 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Elliot Boschwitz on 2/3/24.
+//
+
+import Foundation

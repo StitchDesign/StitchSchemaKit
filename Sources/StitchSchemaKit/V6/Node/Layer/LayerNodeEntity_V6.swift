a4ec32fd17069ce71ba9facf05cbb21f847f5480
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-04-03 12:14:24 -0700

    layernodeentity init

--- Sources/StitchSchemaKit/V6/Node/Layer/LayerNodeEntity_V6.swift
+++ Sources/StitchSchemaKit/V6/Node/Layer/LayerNodeEntity_V6.swift
@@ -62,12 +62,74 @@ public enum LayerNodeEntity_V6: StitchSchemaVersionable {
         public init(id: NodeId,
                     layer: Layer,
                     positionPort: NodeConnectionType,
+                    sizePort: NodeConnectionType,
+                    scalePort: NodeConnectionType,
+                    anchoringPort: NodeConnectionType,
+                    opacityPort: NodeConnectionType,
+                    zIndexPort: NodeConnectionType,
+                    colorPort: NodeConnectionType,
+                    rotationXPort: NodeConnectionType,
+                    rotationYPort: NodeConnectionType,
+                    rotationZPort: NodeConnectionType,
+                    lineColorPort: NodeConnectionType,
+                    lineWidthPort: NodeConnectionType,
+                    blurPort: NodeConnectionType,
+                    blendModePort: NodeConnectionType,
+                    brightnessPort: NodeConnectionType,
+                    colorInvertPort: NodeConnectionType,
+                    contrastPort: NodeConnectionType,
+                    hueRotationPort: NodeConnectionType,
+                    saturationPort: NodeConnectionType,
+                    pivotPort: NodeConnectionType,
+                    enabledPort: NodeConnectionType,
+                    blurRadiusPort: NodeConnectionType,
+                    backgroundColorPort: NodeConnectionType,
+                    isClippedPort: NodeConnectionType,
+                    orientationPort: NodeConnectionType,
+                    paddingPort: NodeConnectionType,
+                    setupModePort: NodeConnectionType,
+                    allAnchorsPort: NodeConnectionType,
+                    cameraDirectionPort: NodeConnectionType,
+                    isCameraEnabledPort: NodeConnectionType,
+                    isShadowsEnabledPort: NodeConnectionType,
                     hasSidebarVisibility: Bool,
                     layerGroupId: NodeId?,
                     isExpandedInSidebar: Bool?) {
             self.id = id
             self.layer = layer
+            
             self.positionPort = positionPort
+            self.sizePort = sizePort
+            self.scalePort = scalePort
+            self.anchoringPort = anchoringPort
+            self.opacityPort = opacityPort
+            self.zIndexPort = zIndexPort
+            self.colorPort = colorPort
+            self.rotationXPort = rotationXPort
+            self.rotationYPort = rotationYPort
+            self.rotationZPort = rotationZPort
+            self.lineColorPort = lineColorPort
+            self.lineWidthPort = lineWidthPort
+            self.blurPort = blurPort
+            self.blendModePort = blendModePort
+            self.brightnessPort = brightnessPort
+            self.colorInvertPort = colorInvertPort
+            self.contrastPort = contrastPort
+            self.hueRotationPort = hueRotationPort
+            self.saturationPort = saturationPort
+            self.pivotPort = pivotPort
+            self.enabledPort = enabledPort
+            self.blurRadiusPort = blurRadiusPort
+            self.backgroundColorPort = backgroundColorPort
+            self.isClippedPort = isClippedPort
+            self.orientationPort = orientationPort
+            self.paddingPort = paddingPort
+            self.setupModePort = setupModePort
+            self.allAnchorsPort = allAnchorsPort
+            self.cameraDirectionPort = cameraDirectionPort
+            self.isCameraEnabledPort = isCameraEnabledPort
+            self.isShadowsEnabledPort = isShadowsEnabledPort
+            
             self.hasSidebarVisibility = hasSidebarVisibility
             self.layerGroupId = layerGroupId
             self.isExpandedInSidebar = isExpandedInSidebar
@@ -81,6 +143,36 @@ extension LayerNodeEntity_V6.LayerNodeEntity: StitchVersionedCodable {
                   layer:
                     LayerNodeEntity_V6.Layer(previousInstance: previousInstance.layer),
                   positionPort: .values([.none]),
+                  sizePort: .values([.none]),
+                  scalePort: .values([.none]),
+                  anchoringPort: .values([.none]),
+                  opacityPort: .values([.none]),
+                  zIndexPort: .values([.none]),
+                  colorPort: .values([.none]),
+                  rotationXPort: .values([.none]),
+                  rotationYPort: .values([.none]),
+                  rotationZPort: .values([.none]),
+                  lineColorPort: .values([.none]),
+                  lineWidthPort: .values([.none]),
+                  blurPort: .values([.none]),
+                  blendModePort: .values([.none]),
+                  brightnessPort: .values([.none]),
+                  colorInvertPort: .values([.none]),
+                  contrastPort: .values([.none]),
+                  hueRotationPort: .values([.none]),
+                  saturationPort: .values([.none]),
+                  pivotPort: .values([.none]),
+                  enabledPort: .values([.none]),
+                  blurRadiusPort: .values([.none]),
+                  backgroundColorPort: .values([.none]),
+                  isClippedPort: .values([.none]),
+                  orientationPort: .values([.none]),
+                  paddingPort: .values([.none]),
+                  setupModePort: .values([.none]),
+                  allAnchorsPort: .values([.none]),
+                  cameraDirectionPort: .values([.none]),
+                  isCameraEnabledPort: .values([.none]),
+                  isShadowsEnabledPort: .values([.none]),
                   hasSidebarVisibility: previousInstance.hasSidebarVisibility,
                   layerGroupId: previousInstance.layerGroupId,
                   isExpandedInSidebar: previousInstance.isExpandedInSidebar)

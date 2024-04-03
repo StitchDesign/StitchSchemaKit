a423eccfa4b2b2038da04e9bb7d57ecde7fb710a
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-04-03 12:09:08 -0700

    schema port key path

--- Sources/StitchSchemaKit/V6/Node/Layer/LayerNodeEntity_V6.swift
+++ Sources/StitchSchemaKit/V6/Node/Layer/LayerNodeEntity_V6.swift
@@ -23,6 +23,36 @@ public enum LayerNodeEntity_V6: StitchSchemaVersionable {
         
         // Value data
         public let positionPort: NodeConnectionType
+        public let sizePort: NodeConnectionType
+        public let scalePort: NodeConnectionType
+        public let anchoringPort: NodeConnectionType
+        public let opacityPort: NodeConnectionType
+        public let zIndexPort: NodeConnectionType
+        public let colorPort: NodeConnectionType
+        public let rotationXPort: NodeConnectionType
+        public let rotationYPort: NodeConnectionType
+        public let rotationZPort: NodeConnectionType
+        public let lineColorPort: NodeConnectionType
+        public let lineWidthPort: NodeConnectionType
+        public let blurPort: NodeConnectionType
+        public let blendModePort: NodeConnectionType
+        public let brightnessPort: NodeConnectionType
+        public let colorInvertPort: NodeConnectionType
+        public let contrastPort: NodeConnectionType
+        public let hueRotationPort: NodeConnectionType
+        public let saturationPort: NodeConnectionType
+        public let pivotPort: NodeConnectionType
+        public let enabledPort: NodeConnectionType
+        public let blurRadiusPort: NodeConnectionType
+        public let backgroundColorPort: NodeConnectionType
+        public let isClippedPort: NodeConnectionType
+        public let orientationPort: NodeConnectionType
+        public let paddingPort: NodeConnectionType
+        public let setupModePort: NodeConnectionType
+        public let allAnchorsPort: NodeConnectionType
+        public let cameraDirectionPort: NodeConnectionType
+        public let isCameraEnabledPort: NodeConnectionType
+        public let isShadowsEnabledPort: NodeConnectionType
         
         // Sidebar data
         public let hasSidebarVisibility: Bool

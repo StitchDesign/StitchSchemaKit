f433c0f75ee01f3532a4557dbae5d740f569bb64
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2024-04-16 15:40:48 -0700

    wip migration fixes

--- Sources/StitchSchemaKit/V7/NodePort/NodePortType_V7.swift
+++ Sources/StitchSchemaKit/V7/NodePort/NodePortType_V7.swift
@@ -10,7 +10,6 @@ import Foundation
 public enum NodeConnectionType_V7: StitchSchemaVersionable {
     // MARK: - ensure versions are correct
     static var version: StitchSchemaVersion = StitchSchemaVersion._V7
-//    public typealias PreviousInstance = Self.NodeConnectionType
     public typealias PreviousInstance = NodeConnectionType_V6.NodeConnectionType
     public typealias PortValues = [PortValue_V7.PortValue]
     public typealias NodeIOCoordinate = NodeIOCoordinate_V7.NodeIOCoordinate
@@ -24,6 +23,11 @@ public enum NodeConnectionType_V7: StitchSchemaVersionable {
 
 extension NodeConnectionType_V7.NodeConnectionType: StitchVersionedCodable {
     public init(previousInstance: NodeConnectionType_V7.PreviousInstance) {
-        fatalError()
+        switch previousInstance {
+        case .values(let portValues):
+            self = .values(NodeConnectionType_V7.PortValues(previousElements: portValues))
+        case .upstreamConnection(let upstreamConnection):
+            self = .upstreamConnection(NodeConnectionType_V7.NodeIOCoordinate(previousInstance: upstreamConnection))
+        }
     }
 }

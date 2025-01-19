d7561cc8790555a0a0236cc896e158df5fe22dd5
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2025-01-19 10:07:22 -0800

    Fixed node position migration logic

--- Sources/StitchSchemaKit/V29/Document/StitchDocument_V29.swift
+++ Sources/StitchSchemaKit/V29/Document/StitchDocument_V29.swift
@@ -9,6 +9,8 @@ import Foundation
 import SwiftUI
 
 public enum StitchDocument_V29: StitchSchemaVersionable {
+    // TODO: remove after version 29
+    static let STITCH_GRAPH_LENGTH: CGFloat = 300000
 
     // MARK: - ensure versions are correct
     public static let version = StitchSchemaVersion._V29
@@ -28,7 +30,7 @@ public enum StitchDocument_V29: StitchSchemaVersionable {
         public let previewWindowBackgroundColor: Color
         
         // Graph positioning data
-        public let localPosition: CGPoint
+        public var localPosition: CGPoint
         public let zoomData: CGFloat
         
         public let cameraSettings: CameraSettings
@@ -53,18 +55,45 @@ public enum StitchDocument_V29: StitchSchemaVersionable {
 
 extension StitchDocument_V29.StitchDocument {
     public init(previousInstance: StitchDocument_V29.PreviousInstance) {
+        // TODO: CHANGE AFTER VERSION 29
         
-        // TODO: remove after version 29
-        var localPosition = previousInstance.localPosition
-        localPosition.x += STITCH_GRAPH_LENGTH/2
-        localPosition.y += STITCH_GRAPH_LENGTH/2
+        var migration = Self.init(graph: .init(previousInstance: previousInstance.graph),
+                                  previewWindowSize: previousInstance.previewWindowSize,
+                                  previewSizeDevice: .init(previousInstance: previousInstance.previewSizeDevice),
+                                  previewWindowBackgroundColor: previousInstance.previewWindowBackgroundColor,
+                                  localPosition: previousInstance.localPosition,
+                                  zoomData: previousInstance.zoomData,
+                                  cameraSettings: .init(previousInstance: previousInstance.cameraSettings))
         
-        self.init(graph: .init(previousInstance: previousInstance.graph),
-                  previewWindowSize: previousInstance.previewWindowSize,
-                  previewSizeDevice: .init(previousInstance: previousInstance.previewSizeDevice),
-                  previewWindowBackgroundColor: previousInstance.previewWindowBackgroundColor,
-                  localPosition: localPosition,
-                  zoomData: previousInstance.zoomData,
-                  cameraSettings: .init(previousInstance: previousInstance.cameraSettings))
+        // Move document to western-most node (same logic as graph centering button)
+        
+        let westernMostPosition = migration.graph.nodes
+            .compactMap {
+                switch $0.nodeTypeEntity {
+                case .patch(let patchNode):
+                    return patchNode.canvasEntity.position
+
+                case .group(let canvas):
+                    return canvas.position
+
+                default:
+                    // we're being lazy not handling layers but all good!
+                    return nil
+                }
+            }
+            .max { n, n2 in
+                n.x > n2.x
+            }
+
+        if let westernMostPosition = westernMostPosition {
+            // Move graph to western most node
+            migration.localPosition = westernMostPosition
+        } else {
+            // Divide by 2 in other scenarios
+            migration.localPosition.x += StitchDocument_V29.STITCH_GRAPH_LENGTH/2
+            migration.localPosition.y += StitchDocument_V29.STITCH_GRAPH_LENGTH/2
+        }
+        
+        self = migration
     }
 }
